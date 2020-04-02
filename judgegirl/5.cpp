ac66a91a7efd67a981098d9be46191894fc8db5f
cies96035 <cies9001005@gmail.com>
2020-04-02 23:15:49 +0800

    jgirl

---
+++ judgegirl/5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int h,w,d;
+	cin>>h>>w>>d;
+	cout<<2*(w*d+h*d+h*w)<<endl<<h*w*d<<endl;
+	return 0;
+}
+
---
+++ judgegirl/6.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	char a;
+	while(cin.get(a)&&a!='\n')
+		cout<<a<<endl;
+	return 0;
+}
+
