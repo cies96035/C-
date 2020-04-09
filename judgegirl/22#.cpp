2cc9345084498b377c23e60791335ad949f0cb47
cies96035 <cies9001005@gmail.com>
2020-04-09 22:13:33 +0800

    add 22#

---
+++ judgegirl/22#.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+ 
+void posneg(int array[5][5], int result[2])
+{
+	for(int i=0;i<5;i++)
+		for(int j=0;j<5;j++)
+			if(array[i][j]<0)result[0]++;
+			else if(array[i][j]>0)result[1]++;
+	return;
+}
+ 
+int main() {
+	  int i, j;
+	  int array[5][5];
+	  int result[2];
+	  for (i = 0; i < 5; i++)
+	    for (j = 0; j < 5; j++)
+	      scanf("%d", &array[i][j]);
+ 
+	  posneg(array, result);
+	  printf("%d\n", result[0]);
+	  printf("%d\n", result[1]);
+	return 0;
+}
+ 
