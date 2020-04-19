8ee981d3fbecada628be25d270ddc30966fe9bfb
cies96035 <cies9001005@gmail.com>
2020-04-19 23:51:47 +0800

    Create 38@.cpp

---
+++ judgegirl/38@.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+int main()
+{
+	int N;
+	cin>>N;
+	while(N--)
+	{
+		int num,power[3],amount[3];
+		cin>>num;
+		map<int,int> dp;
+		for(int i=0;i<3;i++)
+			cin>>amount[i];
+		for(int i=0;i<3;i++)
+			cin>>power[i];
+			
+	}
+	return 0;
+}
+
