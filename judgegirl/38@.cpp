6044ea6ff0cc22c3892aca34d73831b356307486
cies96035 <cies9001005@gmail.com>
2020-04-20 23:59:17 +0800

    Update 38@.cpp

--- judgegirl/38@.cpp
+++ judgegirl/38@.cpp
@@ -2,20 +2,39 @@
 #include<map>
 using namespace std;
 
+int num,power[3],amount[3];
+bool f(int n,int a,int b,int c)
+{
+	//cout<<n<<endl;
+	bool k=0;
+	if(n==0)
+		return 1;
+	if(n<0||(a==0&&b==0&&c==0))
+		return 0;
+	if(a>0)
+		k=k||f(n-power[0],a-1,b,c);
+	if(b>0)
+		k=k||f(n-power[1],a,b-1,c);
+	if(c>0)
+		k=k||f(n-power[2],a,b,c-1);
+	return k;
+}
+
 int main()
 {
 	int N;
 	cin>>N;
 	while(N--)
 	{
-		int num,power[3],amount[3];
 		cin>>num;
-		map<int,int> dp;
 		for(int i=0;i<3;i++)
 			cin>>amount[i];
 		for(int i=0;i<3;i++)
 			cin>>power[i];
-			
+		if(f(num,amount[0],amount[1],amount[2]))
+			cout<<"yes\n";
+		else
+			cout<<"no\n";
 	}
 	return 0;
 }
