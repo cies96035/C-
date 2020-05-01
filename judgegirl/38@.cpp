175fd8979649cf092f737be311eb48d6a1d30eb3
cies96035 <cies9001005@gmail.com>
2020-05-01 22:13:21 +0800

    Update 38@.cpp

--- judgegirl/38@.cpp
+++ judgegirl/38@.cpp
@@ -11,19 +11,38 @@ void Init()
 		two_power[i+1]=two_power[i]*2;
 	return ;
 }
-bool f(int n,int a,int ap,int b,int bp,int c,int cp)//stack overflow
+int t(int n)
 {
+	if(n==0)return -1;
+	int i=30;
+	for(;n<two_power[i];i--);
+	return i;
+}
+
+bool f(int n,int a,int ap,int b,int bp,int c,int cp)
+//bool f(int n,int a,int b,int c)//stack overflow
+{
+	//cout<<n<<endl;
 	bool k=0;
 	if(n==0)
 		return 1;
 	if(n<0||(a==0&&b==0&&c==0))
 		return 0;
 	if(a>0)
-		k=k||f(n-power[0],a-1,b,c);
+	{
+		for(int i=ap;i>=0;i++)
+			k=k||f(n-power[0]*two_power[i],a-two_power[i],i-1,b,bp,c,cp);
+	}
 	if(b>0)
-		k=k||f(n-power[1],a,b-1,c);
+	{
+		for(int i=bp;i>=0;i++)
+			k=k||f(n-power[1]*two_power[i],a,ap,b-two_power[i],i-1,c,cp);
+	}
 	if(c>0)
-		k=k||f(n-power[2],a,b,c-1);
+	{
+		for(int i=cp;i>=0;i++)
+			k=k||f(n-power[2]*two_power[i],a,ap,b,bp,c-two_power[i],i-1);
+	}
 	return k;
 }
 
@@ -41,8 +60,7 @@ int main()
 		}
 		for(int i=0;i<3;i++)
 			cin>>power[i];
-		if(sum>30000)
-		if(f(num,amount[0],amount[1],amount[2]))
+		if(f(num,amount[0],t(amount[0]),amount[1],t(amount[1]),amount[2],t(amount[2])))
 			cout<<"yes\n";
 		else
 			cout<<"no\n";
