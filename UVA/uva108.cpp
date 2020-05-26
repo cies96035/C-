17fad1ec444874c6d4598aace60f14c98a2bc6a7
cies96035 <cies9001005@gmail.com>
2020-05-26 17:36:54 +0800

    Update uva108.cpp

--- UVA/uva108.cpp
+++ UVA/uva108.cpp
@@ -1,28 +1,43 @@
 #include<iostream>
 using namespace std;
 
-#define MaxN 105
-//the maximum N is very small, so can just force
-int arr[MaxN][MaxN];
-int Max=-10000,num,N;
+
+//the maxN is too small so it can just force
+
+#define maxN 110
+int N;
+int sum[maxN][maxN],Max;
+
+inline int getsum(int u,int l,int d,int r)
+{
+	return sum[d][r]-sum[u-1][r]-sum[d][l-1]+sum[u-1][l-1];
+}
 int main()
 {
-	cin>>N;
-	for(int i=0;i<N;i++)
-		for(int j=0;j<N;j++)
-			cin>>arr[i][j];
-	for(int i=0;i<N;i++)
-		for(int j=0;j<N;j++)
+	cin.tie(0);
+	ios_base::sync_with_stdio(0);
+	while(cin>>N)
+	{
+		Max=-2e9;
+		for(int i=1;i<=N;i++)
 		{
-			num=0;
-			for(int k=i;k<N;k++)
-				for(int l=j;l<N;l++)
-				{
-					num+=arr[k][j];
-					if(num>Max)
-						Max=num;
-				}
+			for(int j=1;j<=N;j++)
+			{
+				cin>>sum[i][j];
+				sum[i][j]+=sum[i-1][j]+sum[i][j-1]-sum[i-1][j-1];
+			}
 		}
-	cout<<Max<<endl;
+		for(int u=1;u<=N;u++)
+			for(int l=1;l<=N;l++)
+			{
+				for(int d=u;d<=N;d++)
+					for(int r=l;r<=N;r++)
+					{
+						if(getsum(u,l,d,r)>Max)Max=getsum(u,l,d,r);
+					}
+			}
+		cout<<Max<<'\n';
+	}
 	return 0;
 }
+
