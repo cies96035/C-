5c8f693f01a9f8e52d3d52ec6d740d16f8072cf4
cies96035 <cies9001005@gmail.com>
2020-07-02 15:29:41 +0800

    autoCommit 2 週四0/20.152940

--- TOJ/已註解,整碼/toj16.cpp
+++ TOJ/已註解,整碼/toj16.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
-#include<cstring> 
+#include<cstring>
 using namespace std;
 int main()
 {
-    //cin加速 
+    //cin加速
     cin.tie(0);
     ios_base::sync_with_stdio(0);
-    
-    //T筆測資 
+
+    //T筆測資
     int T;
     cin>>T;
     while(T--)
@@ -15,19 +15,19 @@ int main()
         int S,Q,W;
 		bool cantake[30000];
         memset(cantake,0,sizeof(cantake));
-        
-        //輸入心情指數 
+
+        //輸入心情指數
         cin>>S;
         int emotion[S];
         for(int i=0;i<S;i++)cin>>emotion[i];
-        
+
         //處理心情指數
         for(int a=0;a<S;a++)
             for(int b=a+1;b<S;b++)
                 if(emotion[a]&&emotion[b]&&!(cantake[emotion[a]+emotion[b]]))
                     cantake[emotion[a]+emotion[b]]=1;
-          
-        //判斷並輸出 
+
+        //判斷並輸出
         cin>>Q;
         while(Q--)
         {
@@ -35,7 +35,7 @@ int main()
             if(cantake[W])cout<<"Good!\n";
             else cout<<"So Bad!\n";
         }
-        
+
     }
-    return 0;
+    return 414314;
 }
--- TOJ/已註解,整碼/toj9.cpp
+++ TOJ/已註解,整碼/toj9.cpp
@@ -6,37 +6,37 @@ int main()
 {
 	cin.tie(0);
 	ios_base::sync_with_stdio(0);
-	
+
     int T,M,N,S;
     cin>>T;
     for(int Case=1;Case<=T;Case++)
     {
-    	//輸入幾個點，幾個連結，起點 
+    	//輸入幾個點，幾個連結，起點
         cin>>N>>M>>S;
-        
+
         /*
-        //變數用途 
+        //變數用途
         High:各點高度
 		far:紀錄原點到各點最近的距離(DP找出最低點最近距離
 		walk:原點到最低點最近距離(無法到達則沒有值
 		down:下坡車距離
-		end:紀錄最低點高度(不用座標是因為可能有多個最低點 
+		end:紀錄最低點高度(不用座標是因為可能有多個最低點
 		graph:雙向圖
 		isgo:BFS紀錄已通過的點，避免走回頭路
-		f1,f2:分別判斷走路和下坡車能不能到達 
+		f1,f2:分別判斷走路和下坡車能不能到達
 		*/
 		queue<int> togo;//BFS用到
         int High[N],far[N],walk,down=0,end=2e9;
         bool graph[N][N],isgo[N],f1=0,f2=1;
-        //清空陣列 
+        //清空陣列
         memset(graph,0,sizeof(graph));
         memset(isgo,0,sizeof(isgo));
-        
-        //輸入每個座標高度、雙向圖 
+
+        //輸入每個座標高度、雙向圖
         for(int i=0;i<N;i++)
         {
             cin>>High[i];
-            //紀錄最低點高度 
+            //紀錄最低點高度
             if(end>High[i])
             {
                 end=High[i];
@@ -48,19 +48,19 @@ int main()
             cin>>x>>y;
             graph[x][y]=graph[y][x]=1;
         }
-        
-        togo.push(S);//把起點放入搜尋佇列 
-        isgo[S]=1;//把起點放入已探索名單 
-        far[S]=0;//從起點到起點的距離為0 
-        while(!togo.empty())//直到搜尋佇列空空為止 
+
+        togo.push(S);//把起點放入搜尋佇列
+        isgo[S]=1;//把起點放入已探索名單
+        far[S]=0;//從起點到起點的距離為0
+        while(!togo.empty())//直到搜尋佇列空空為止
         {
             int t=togo.front();
             togo.pop();
-            //將搜尋佇列的第一個拿出來，開始搜尋相鄰的點 
-            if(High[t]==end) 
+            //將搜尋佇列的第一個拿出來，開始搜尋相鄰的點
+            if(High[t]==end)
             {
                 //如果出現了第一個由起點連到最低點的路徑
-                //則f1為1(走路可到達)，且距離為此最低點的far 
+                //則f1為1(走路可到達)，且距離為此最低點的far
                 f1=1;
                 walk=far[t]*6;
                 break;
@@ -72,23 +72,23 @@ int main()
                 {
                     togo.push(i);
                     isgo[i]=1;
-                    far[i]=far[t]+1;//紀錄起點到每個點的最短距離 
-                    
+                    far[i]=far[t]+1;//紀錄起點到每個點的最短距離
+
                 }
             }
         }
-        
+
         //下坡車，用while直到高度到最低點才停止
-        //理論上要用BFS(如果同時有兩個以上最低點，此程式只會到其中一個) 
+        //理論上要用BFS(如果同時有兩個以上最低點，此程式只會到其中一個)
         while(High[S]!=end)
         {
             /*
             min:紀錄最低點高度
-            minid:記錄最低點id(下次從這裡出發)，初始值為-1(找不到跟出發點相連且比出發點低的點 
-            */ 
+            minid:記錄最低點id(下次從這裡出發)，初始值為-1(找不到跟出發點相連且比出發點低的點
+            */
             int min=High[S],minid=-1;
-            
-            //掃過每一個點，找出比出發點低最多的那個點，記錄下來 
+
+            //掃過每一個點，找出比出發點低最多的那個點，記錄下來
             for(int i=0;i<N;i++)
             {
                 if(graph[i][S]&&min>High[i])
@@ -97,22 +97,22 @@ int main()
                     minid=i;
                 }
             }
-            
-            //如果與出發點相連的點都比較高，則跳開迴圈，並記錄下坡車無法到達 
+
+            //如果與出發點相連的點都比較高，則跳開迴圈，並記錄下坡車無法到達
             if(minid==-1)
             {
                 f2=0;
                 break;
             }
-            
-            //下次的出發點為這次的最低點 
+
+            //下次的出發點為這次的最低點
             S=minid;
-            //每下坡車一次，down就增加一次 
+            //每下坡車一次，down就增加一次
             down++;
         }
-        
-        
-        //輸出部分 
+
+
+        //輸出部分
         if(f1&&f2)
             cout<<"Case #"<<Case<<": "<<walk-down<<'\n';
         else if(f1)
