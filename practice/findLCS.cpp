473ef787416a26d390b5ac2a4c14af8390ca6e51
cies96035 <39860649+cies96035@users.noreply.github.com>
2020-12-11 23:49:52 +0800

    Update findLCS.cpp

--- practice/findLCS.cpp
+++ practice/findLCS.cpp
@@ -6,36 +6,41 @@ using namespace std;
 #define maxS 1000
 
 
+#define Test //comment if you want to test
+
 /*
-  (0,0) - > j
+  (0,0) - > j strB
     |
     v
-    i
+  i strA
 */
+
 
 struct DP
 {
-    bool fromLeft;
-    bool fromUpLeft;
-    bool fromUp;
-    int LCSLength;
+    bool fromLeft;   //Is the LCSLength come from Left
+    bool fromUpLeft; //Is the LCSLength come from UpLeft
+    bool fromUp;     //Is the LCSLength come from Up
+    int LCSLength;   //The LCSLength
 };
-
-set<string> setLCS;
 DP dp[maxS][maxS];
-string A, B;
+
+string A, B; //two input string
+set<string> setLCS; //recording LCS
 
 void findLCS(int i, int j, string str)
 {
-    //cout<<i<<j<<endl;
-    //cout<<S.fromLeft<<S.fromUp<<S.fromUpLeft<<endl;
-    //cout<<s<<endl;
-    DP S=dp[i + 1][j + 1];
+    DP S=dp[i][j];
+
+    #ifdef Test
+    cout << "findLCS(" << i << ',' << j << ",\"" << str << "\")" << endl;
+    cout << S.fromLeft << " , " << S.fromUp << " , " << S.fromUpLeft << endl;
+    #endif
 
     if(S.LCSLength==0)
     {
         setLCS.insert(str);
-        return;
+        //return;
     }
 
     if(S.fromUp)
@@ -53,7 +58,7 @@ int main()
     cin.tie(0);
     ios_base::sync_with_stdio(0);
 
-    //memset(N, 0, sizeof(S) * maxS * maxS);
+    //memset(dp, 0, sizeof(DP) * maxS * maxS);
 
     cin >> A >> B;
     for(int i = 0 ; i < A.size() ; i++)
@@ -61,34 +66,53 @@ int main()
         {
             if(A[i] == B[j])
             {
+                #if defined(Test)
+                    cout << "A[" << i << "] == " << "B[" << j << "]"<<endl;
+                #endif
                 dp[i + 1][j + 1].fromUpLeft = true;
                 dp[i + 1][j + 1].LCSLength = dp[i][j].LCSLength + 1;
             }
             else
             {
+                #if defined(Test)
+                    cout << "A[" << i << "] != " << "B[" << j << "]"<<endl;
+                #endif
+
                 const int 
                 UL = dp[i][j].LCSLength,
-                U  = dp[i][j+1].LCSLength,
-                L  = dp[i+1][j].LCSLength;
+                U  = dp[i][j + 1].LCSLength,
+                L  = dp[i + 1][j].LCSLength;
 
                 const int Max=max(max(U , L) , UL);
-                if(UL == Max && !(U == Max || L == Max) )
-                    dp[i+1][j+1].fromUpLeft = true;
-                if(U == Max)
-                    dp[i+1][j+1].fromUp = true;
-                if(L == Max)
-                    dp[i+1][j+1].fromLeft = true;
                 
-                dp[i+1][j+1].LCSLength = Max;
+                if(UL == Max && !(U == Max || L == Max) )
+                    dp[i + 1][j + 1].fromUpLeft = true;
+                if(U == Max)
+                    dp[i + 1][j + 1].fromUp = true;
+                if(L == Max)
+                    dp[i + 1][j + 1].fromLeft = true;
+                
+                dp[i + 1][j + 1].LCSLength = Max;
             }
+            #if defined(Test)
+                cout << "dp[" << i + 1 << "][" << j + 1 << "] from:"<<endl;
+                if(dp[i][j].fromLeft)
+                    cout << "Left ";
+                if(dp[i][j].fromUp)
+                    cout << "Up ";
+                if(dp[i][j].fromUpLeft)
+                    cout << "UpLeft ";
+                cout << endl;
+            #endif
         }
-        cout << "Max Length: " << dp[ A.size() ][ B.size() ].LCSLength << '\n';
+        cout << "LCS Length: " << dp[A.size()][B.size()].LCSLength << '\n';
 
         findLCS(A.size(), B.size(), "");
 
         cout << "LCS amounts: " << setLCS.size() << '\n';
 
+        int id = 0;
         for(set<string>::iterator i = setLCS.begin() ; i != setLCS.end() ; i++)
-            cout << *i << '\n';
+            cout << id++ << " : " << *i << '\n';
         return 0;
 }
